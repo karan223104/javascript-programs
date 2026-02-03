ae6c875a92eb91ad37087d47614cb6102538a00e
Karan Chauhan <kc8140898189@gmail.com>
2026-02-03 18:00:08 +0530

    add comments

--- variale.js
+++ variale.js
@@ -1,12 +1,12 @@
 console.log("karan chauhan");
 
-fullName = "karan";
-age = 32;
-price = 99;
-radius = 3.9;
-a = null;
-y = undefined;
-isMarried = false;
+fullName = "karan";         // string
+age = 32;                   // number
+price = 99;                 // number
+radius = 3.9;               // number
+a = null;                   // null
+y = undefined;              // undefined  
+isMarried = false;          // boolean
 
-console.log(fullName);
+console.log(fullName);      // print variable value
 console.log(age);
